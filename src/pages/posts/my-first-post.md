b9aeaed801c95e652d3ef8c0fc09f053e6fe4d9e
BillyTi <billy.ti@dotdot.cc>
2024-08-16 22:12:50 +0800

    add : loading component

--- src/pages/posts/my-first-post.md
+++ src/pages/posts/my-first-post.md
@@ -1,12 +1,17 @@
 ---
-layout: "../../layouts/Layout.astro"
-title: "My First Post"
+# layout: "../../layouts/Layout.astro"
+layout: "../../layouts/MarkdownLayout.astro"
 date: "2024-07-31"
+title: "My First Post1"
 ---
 
 # My First Post
 
 This is the content of my first post. Welcome to my blog!
+
+- e12121
+- 121212
+
 ```javascript
 function setInitialTheme() {
   const storedTheme = localStorage.getItem("theme");
@@ -15,7 +20,6 @@ function setInitialTheme() {
     document.documentElement.classList.add("dark");
   } else {
     document.documentElement.classList.remove("dark");
-  }
   toggleIcons(isDarkMode);
 }
 ```
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,25 @@ module.exports = {
     './src/**/*.{astro,html,js,jsx,ts,tsx,vue}',
     './public/**/*.html',
   ],
+  corePlugins: {
+    preflight: false, // 禁用 Tailwind 的全局樣式重置 (即 preflight)
+  },
   darkMode: 'class', // 啟用基於 class 的暗黑模式
   theme: {
     extend: {
       filter: {
         'icon-border': 'drop-shadow(0 0 2px white)',
-      }
+      },
+      keyframes: {
+        flip: {
+          '0%': { transform: 'scaleX(1)' },   // 初始狀態，正常顯示
+          '50%': { transform: 'scaleX(-1)' }, // 鏡像反轉
+          '100%': { transform: 'scaleX(1)' }  // 回到正常顯示
+        },
+      },
+      animation: {
+        'flip': 'flip 2s ease-in-out infinite', // 鏡像反轉動畫，持續 2 秒，無限循環
+      },
     },
   },
   plugins: [],
